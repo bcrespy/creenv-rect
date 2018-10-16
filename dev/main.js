25b9cc7a571e5568e2b570c0946819ea802843b4
Baptiste Crespy <baptiste.crespy@gmail.com>
2018-10-16 18:16:46 +0200

    first tests

--- dev/main.js
+++ dev/main.js
@@ -1,8 +1,1 @@
-import Color from '../lib/index';
-
-
-let color = new Color([255,10,10]);
-color.r = 50;
-
-let c = Color.fromString("#ffffff");
-console.log(c)
+import Colorpicker from '../lib/index';
