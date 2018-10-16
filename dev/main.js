d78f9765c8ea834cfd42155b4be80764ac919296
Baptiste Crespy <baptiste.crespy@gmail.com>
2018-10-16 02:20:09 +0200

    color now extends Vector, allowing operations

--- dev/main.js
+++ dev/main.js
@@ -1,8 +1,8 @@
-import Color from '../lib/index.es6';
+import Color from '../lib/index';
 
 
 let color = new Color([255,10,10]);
 color.r = 50;
 
-let c = Color.fromString("#eiojzeoijfef");
+let c = Color.fromString("#ffffff");
 console.log(c)
