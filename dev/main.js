03e85bd61040a93651c5852299eb870c57f828b7
Baptiste Crespy <baptiste.crespy@gmail.com>
2018-10-19 01:54:51 +0200

    who's gonna read this anyway ?

--- dev/main.js
+++ dev/main.js
@@ -1,3 +1,18 @@
 import Colorpicker from '../lib/index';
+import Color from '@creenv/color';
 
-let cp = new Colorpicker();
+let cp = new Colorpicker(new Color(220, 0, 240), {
+  suggestions: [
+    new Color(30, 150, 20),
+    new Color(255, 0, 255, 0.4),
+    new Color(0, 255, 0)
+  ]
+});
+
+let co = new Colorpicker(new Color(220, 0, 240), {
+  suggestions: [
+    new Color(30, 150, 20),
+    new Color(255, 0, 255, 0.4),
+    new Color(0, 255, 0)
+  ]
+});
