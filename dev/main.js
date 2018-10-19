0431fcf4fb025f3a6a7c39914c8f127e0cb1fb25
Baptiste Crespy <baptiste.crespy@gmail.com>
2018-10-19 08:19:33 +0200

    added _onChange callback call

--- dev/main.js
+++ dev/main.js
@@ -1,7 +1,7 @@
 import Colorpicker from '../lib/index';
 import Color from '@creenv/color';
 
-let cp = new Colorpicker(new Color(220, 0, 240), {
+let cp = new Colorpicker(new Color(220, 0, 240), (color) => {console.log(color.rgb); }, {
   suggestions: [
     new Color(30, 150, 20),
     new Color(255, 0, 255, 0.4),
@@ -9,10 +9,4 @@ let cp = new Colorpicker(new Color(220, 0, 240), {
   ]
 });
 
-let co = new Colorpicker(new Color(220, 0, 240), {
-  suggestions: [
-    new Color(30, 150, 20),
-    new Color(255, 0, 255, 0.4),
-    new Color(0, 255, 0)
-  ]
-});
+document.body.appendChild(cp.dom);
