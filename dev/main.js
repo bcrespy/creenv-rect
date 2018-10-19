4e5f4124a2be5cac05fae14e166f4b26a9fd0ad9
Baptiste Crespy <baptiste.crespy@gmail.com>
2018-10-19 15:46:05 +0200

    added a direction on display

--- dev/main.js
+++ dev/main.js
@@ -6,7 +6,8 @@ let cp = new Colorpicker(new Color(220, 0, 240), (color) => {console.log(color.r
     new Color(30, 150, 20),
     new Color(255, 0, 255, 0.4),
     new Color(0, 255, 0)
-  ]
+  ],
+  direction: Colorpicker.DIRECTION.TOP_RIGHT
 });
 
 document.body.appendChild(cp.dom);
